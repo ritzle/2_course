efddcf008fd484af9c8d97463059c16c4aa83b06
ritzle <vladislav.ryazanov.study@gmail.com>
2024-12-18 13:08:35 +0700

    обработка ошибок при запуске

--- programming/labs/lab5/main.cpp
+++ programming/labs/lab5/main.cpp
@@ -12,40 +12,39 @@ int main(int argc, char* argv[]) {
     try {
       n = stoi(argv[1]);
       bombCount = stoi(argv[2]);
-      if (n <= 0 || bombCount <= 0 || bombCount > n * n) {
-        throw invalid_argument("Некорректные параметры!");
+      if (n <= 0 || bombCount <= 0 || bombCount > n * n || n > 60) {
+        throw invalid_argument("Некорректные параметры! поле макс 60");
       }
+
+      srand(static_cast<unsigned>(time(0)));
+
+      const int fixedWindowWidth = 600;  // Фиксированная ширина окна
+      const int fixedWindowHeight = 600;  // Фиксированная высота окна
+      int yOffset = 60;  // Смещение игрового поля вниз
+
+      // Рассчитываем размер клетки и размеры окна
+      int cellSize =
+          std::min(fixedWindowWidth / n, (fixedWindowHeight - yOffset) / n);
+      const int windowWidth = cellSize * n;
+      const int windowHeight = cellSize * n + yOffset;
+
+      // Создание окна
+      sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight),
+                              "Minesweeper");
+
+      // Создание менеджера игры
+      GameManager gameManager(window, n, bombCount, cellSize, yOffset);
+
+      // Запуск игры
+      gameManager.run();
+
     } catch (const exception& e) {
       cerr << "Ошибка: " << e.what() << endl;
-      cerr << "Используются значения по умолчанию: поле 10x10, бомб 10."
-           << endl;
     }
   } else if (argc != 1) {
     cerr << "Неверное количество параметров. Используются значения по умолчанию"
          << endl;
   }
-
-  srand(static_cast<unsigned>(time(0)));
-
-  const int fixedWindowWidth = 600;  // Фиксированная ширина окна
-  const int fixedWindowHeight = 600;  // Фиксированная высота окна
-  int yOffset = 60;  // Смещение игрового поля вниз
-
-  // Рассчитываем размер клетки и размеры окна
-  int cellSize =
-      std::min(fixedWindowWidth / n, (fixedWindowHeight - yOffset) / n);
-  const int windowWidth = cellSize * n;
-  const int windowHeight = cellSize * n + yOffset;
-
-  // Создание окна
-  sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight),
-                          "Minesweeper");
-
-  // Создание менеджера игры
-  GameManager gameManager(window, n, bombCount, cellSize, yOffset);
-
-  // Запуск игры
-  gameManager.run();
 
   return 0;
 }
